ee0954f7836bfd4665fa6ddf132c020215a07552
hean3331822689-lang <hean3331822689@gmail.com>
2026-07-27 00:47:35 +0800

    Render site from formula spec code

---
+++ mathjax.js
@@ -0,0 +1,7 @@
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  script.async = true;
+  script.id = "MathJax-cdn-script";
+  document.head.appendChild(script);
+})();
