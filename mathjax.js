d3bd3e2eb0ff7ccbae48def91e3c29a32aa9c192
hean3331822689-lang <hean3331822689@gmail.com>
2026-09-22 00:41:14 +0800

    Apply final identity gate requirements

--- mathjax.js
+++ mathjax.js
@@ -1,7 +1,7 @@
 (function () {
   var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  script.src = "mathjax-local/es5/tex-chtml.js";
   script.async = true;
-  script.id = "MathJax-cdn-script";
+  script.id = "MathJax-local-script";
   document.head.appendChild(script);
 })();
